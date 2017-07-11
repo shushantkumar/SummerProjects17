2bf8d09f7e00a829913debbb3277528b634305d1
Shushant Kumar <Shushant Kumar>
2017-07-11 12:17:46 +0530

    Changes made

--- FP-Erlang/Week 6/Assignment 5/Shushant/currying.erl
+++ FP-Erlang/Week 6/Assignment 5/Shushant/currying.erl
@@ -1,22 +1,44 @@
 -module(currying).
 -export([add/1,sub/1,mult/1,divi/1,greaterThanX/1,lessThanX/1]).
+-export([add/2,sub/2,mult/2,divi/2,greaterThanX/2,lessThanX/2]).
+
 
 add(X)-> fun(Y)->X +Y end.
+add(X,Y)->X+Y.
+
 sub(X)-> fun(Y)->X -Y end.
+sub(X,Y)->X-Y.
+
 mult(X)-> fun(Y)->X *Y end.
+mult(X,Y)->X*Y.
+
 divi(X)-> fun(Y)->X /Y end.
+divi(X,Y)->X/Y.
+
 greaterThanX(X)->fun(Y)->
 if
 X<Y->true; 
 true->false
 end 
 end.
+greaterThanX(X,Y)->if
+X<Y->true; 
+true->false
+end.
+
+
 lessThanX(X)->fun(Y)->
 if 
 X>Y->true; 
 true->false 
 end
 end.
+lessThanX(X,Y)->
+if 
+X>Y->true; 
+true->false 
+end.
+
 
 %% Examples of curried functions in the classic higher order functions like map, filter and fold %%
 %% Even = fun(X) -> (X rem 2) =:= 0 end. 
